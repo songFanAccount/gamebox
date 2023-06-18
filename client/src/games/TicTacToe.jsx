b26459101418217ec8cdce37acbe2b6e33ed61c4
Daniel Song <z5260059@ad.unsw.edu.au>
2023-06-18 15:14:52 +1000

    tictactoe init

--- client/src/games/TicTacToe.jsx
+++ client/src/games/TicTacToe.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TicTacToe() {
+    const [board, setBoard] = useState([
+        [-1, -1, -1],
+        [-1, -1, -1],
+        [-1, -1, -1]
+    ])
     return (
         <div>TicTacToe</div>
     )
