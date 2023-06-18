1eb67e370cb4f8d091603091bf844c882e415e12
Daniel Song <z5260059@ad.unsw.edu.au>
2023-06-18 15:16:36 +1000

    more init

--- client/src/games/TicTacToe.jsx
+++ client/src/games/TicTacToe.jsx
@@ -1,12 +1,17 @@
+import { Box } from '@mui/material'
 import React, { useState } from 'react'
 
 export default function TicTacToe() {
+    const socket = global.socket
     const [board, setBoard] = useState([
         [-1, -1, -1],
         [-1, -1, -1],
         [-1, -1, -1]
     ])
     return (
-        <div>TicTacToe</div>
+        <Box>
+            <div>TicTacToe</div>
+            {board}
+        </Box>
     )
 }
