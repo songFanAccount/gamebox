85a73873b6932ff790bb02974fe5a08ca75fa73b
Daniel Song <z5260059@ad.unsw.edu.au>
2023-06-23 17:36:42 +1000

    Win anim added

--- client/src/games/TicTacToe.jsx
+++ client/src/games/TicTacToe.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Stack } from '@mui/material'
 import React, { useState } from 'react'
+import { motion } from "framer-motion";
 import { GBText } from '../components/generalComponents'
 import CloseIcon from '@mui/icons-material/Close';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
@@ -14,6 +15,10 @@ export default function TicTacToe() {
     const [emptySpaces, setEmptySpaces] = useState(9)
     const [turn, setTurn] = useState(-1) // -1 for X, 1 for O
     const [winner, setWinner] = useState(0) // -1 for X, 1 for O, 0 for undetermined
+    const [rowWin, setRowWin] = useState(-1) // -1 for no row win, otherwise 0,1,2 for which row won
+    const [colWin, setColWin] = useState(-1) // -1 for no column win, otherwise 0,1,2 for which column won
+    const [leftDiagWin, setLeftDiagWin] = useState(false)
+    const [rightDiagWin, setRightDiagWin] = useState(false)
     function clickSquare(rowIndex, colIndex) {
         const newBoardState = 
             board.map((row, rIndex) => (
@@ -43,8 +48,14 @@ export default function TicTacToe() {
         const rightDiagWin = 
             newBoardState[0][2] === turn &&
             newBoardState[1][1] === turn &&
-            newBoardState[2][0] === turn 
+            newBoardState[2][0] === turn
+        // Determine if won
         const win = rowWin || colWin || leftDiagWin || rightDiagWin
+        /* Update win condition states */
+        if(rowWin) setRowWin(rowIndex)
+        if(colWin) setColWin(colIndex)
+        setLeftDiagWin(leftDiagWin)
+        setRightDiagWin(rightDiagWin)
         if(win) setWinner(turn)
         else setTurn(-turn)
     }
@@ -81,20 +92,79 @@ export default function TicTacToe() {
             )
         }
     }
+    const draw = {
+        hidden: { pathLength: 0, opacity: 0, stroke: "#FFFFFF", strokeWidth: 5 },
+        visible: {
+            pathLength: 1,
+            opacity: 1,
+            transition: {
+              pathLength: { type: "tween", duration: 0.5, bounce: 0 },
+              opacity: { duration: 0.01 }
+            }
+        }
+      };
     return (
         <Box>
             <GBText text="Tic Tac Toe"/>
             <GameStatus/>
             <Stack direction="column"
                 sx={{
-                    width: 'fit-content',
-                    border: 1, borderColor: '#FFFFFF', boxSizing: 'border-box'
+                    width: 300, height: 300,
+                    border: 1, borderColor: '#FFFFFF'
                 }}
             >
+                <Box
+                    component={motion.svg}
+                    initial="hidden"
+                    animate="visible"
+                    sx={{
+                        width: 300, minHeight: 300,
+                        border: 1, borderColor: '#FFFFFF', boxSizing: 'border-box',
+                        position: 'absolute',
+                    }}
+                >
+                    {rowWin !== -1 && 
+                        <motion.line
+                            x1="18"
+                            y1={50 + rowWin * 100}
+                            x2="282"
+                            y2={50 + rowWin * 100}
+                            variants={draw}
+                        />
+                    }
+                    {colWin !== -1 && 
+                        <motion.line
+                            x1={49 + colWin * 100}
+                            y1="18"
+                            x2={49 + colWin * 100}
+                            y2="282"
+                            variants={draw}
+                        />
+                    }
+                    {leftDiagWin && 
+                        <motion.line
+                            x1="20"
+                            y1="20"
+                            x2="280"
+                            y2="280"
+                            variants={draw}
+                        />
+                    }
+                    {rightDiagWin && 
+                        <motion.line
+                            x1="278"
+                            y1="20"
+                            x2="18"
+                            y2="280"
+                            variants={draw}
+                        />
+                    }
+                </Box>
                 {board.map((row, rowIndex)=> (
                     <Stack direction="row">
                         {row.map((el, colIndex) => (
                             <Button
+                                zIndex={0}
                                 className={`tictactoe-${rowIndex}-${colIndex}`}
                                 disableRipple
                                 disabled={el !== 0 || winner !== 0}
@@ -102,11 +172,10 @@ export default function TicTacToe() {
                                 sx={{
                                     p:0, m:0, 
                                     width: squareWidth, height: squareWidth,
-                                    backgroundColor: '#121212',
                                     border: 1, borderColor: '#FFFFFF', boxSizing: 'border-box', borderRadius: 0,
                                     display: 'flex', justifyContent: 'center', alignItems: 'center',
                                     '&:hover': {
-                                        backgroundColor: '#121212',
+                                        backgroundColor: 'transparent',
                                     }
                                 }}
                             >
@@ -116,7 +185,6 @@ export default function TicTacToe() {
                     </Stack>
                 ))}
             </Stack>
-            {board}
         </Box>
     )
 }
