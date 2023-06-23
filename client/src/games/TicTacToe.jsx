2494cc2a51af1125923f1b2240a94612bc12f240
Daniel Song <z5260059@ad.unsw.edu.au>
2023-06-23 14:57:45 +1000

    board display mostly done

--- client/src/games/TicTacToe.jsx
+++ client/src/games/TicTacToe.jsx
@@ -1,18 +1,68 @@
-import { Box } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 import React, { useState } from 'react'
+import { GBText } from '../components/generalComponents'
+import CloseIcon from '@mui/icons-material/Close';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
 export default function TicTacToe() {
-    //eslint-disable-next-line
     const socket = global.socket
-    //eslint-disable-next-line
+    // const [board, setBoard] = useState([
+    //     [0, 0, 0],
+    //     [0, 0, 0],
+    //     [0, 0, 0]
+    // ])
     const [board, setBoard] = useState([
-        [-1, -1, -1],
-        [-1, -1, -1],
-        [-1, -1, -1]
+        [0, 1, -1],
+        [1, 0, 1],
+        [0, -1, 0]
     ])
+    const squareWidth = 100
+    const Element = ({el}) => {
+        /* el can be -1 (X), 0 (None), or 1 (O) */
+        const crossSx = {
+            color: '#FFFFFF',
+            fontSize: 60
+        }
+        const circleSx = {
+            color: '#FFFFFF',
+            fontSize: 50
+        }
+        switch(el) {
+            case 0:
+                return <></>
+            case -1:
+                return <CloseIcon sx={crossSx}/>
+            case 1: 
+                return <RadioButtonUncheckedIcon sx={circleSx}/>
+            default:
+                throw new Error('TicTacToe: Unexpected element!')
+        }
+    }
     return (
         <Box>
-            <div>TicTacToe</div>
+            <GBText text="Tic Tac Toe"/>
+            <Stack direction="column"
+                sx={{
+                    width: 'fit-content',
+                    border: 1, borderColor: '#FFFFFF', boxSizing: 'border-box'
+                }}
+            >
+                {board.map((row)=> (
+                    <Stack direction="row">
+                        {row.map((el) => (
+                            <Box
+                                sx={{
+                                    width: squareWidth, height: squareWidth, 
+                                    border: 1, borderColor: '#FFFFFF', boxSizing: 'border-box',
+                                    display: 'flex', justifyContent: 'center', alignItems: 'center'
+                                }}
+                            >
+                                <Element el={el}/>
+                            </Box>
+                        ))}
+                    </Stack>
+                ))}
+            </Stack>
             {board}
         </Box>
     )
