ee97c73323d39ebeaccd2c6415bbfc2031e426e8
harryJang <yunseokbb@gmail.com>
2023-07-12 22:57:12 +0900

    recommend button added. Currently just console logging a messeage.

--- client/src/layouts/GameLayoutComponents/GameButton.jsx
+++ client/src/layouts/GameLayoutComponents/GameButton.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 import { GBButton } from "../../components/generalComponents"
+import { Box, IconButton } from "@mui/material"
 
 export default function GameButton({gameName, onClick, isHost}) {
     return (
-        <GBButton children={gameName} onClick={() => onClick(gameName)} disabled={!isHost}/>
+        <Box>
+            <GBButton children={gameName} onClick={() => onClick(gameName)} disabled={!isHost}/>
+            <GBButton children={"recommend"} onClick={() => console.log("added to 'to play next'")}/>
+            {/* <IconButton  onClick={() => console.log("Moved into 'to play next'")}/> */}
+        </Box>
     )
 }
