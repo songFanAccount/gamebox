3c2fa9f192de110c7c760c9832dea2097713f2c5
harryJang <yunseokbb@gmail.com>
2023-07-12 23:08:59 +0900

    changed recommend button to an icon button

--- client/src/layouts/GameLayoutComponents/GameButton.jsx
+++ client/src/layouts/GameLayoutComponents/GameButton.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { GBButton } from "../../components/generalComponents"
 import { Box, IconButton } from "@mui/material"
+import RecommendIcon from '@mui/icons-material/Recommend';
 
 export default function GameButton({gameName, onClick, isHost}) {
     return (
         <Box>
             <GBButton children={gameName} onClick={() => onClick(gameName)} disabled={!isHost}/>
-            <GBButton children={"recommend"} onClick={() => console.log("added to 'to play next'")}/>
-            {/* <IconButton  onClick={() => console.log("Moved into 'to play next'")}/> */}
+            <IconButton aria-label="recommend" color="primary" onClick={() => console.log('recommending '+gameName)}>
+                <RecommendIcon/>
+            </IconButton>
         </Box>
     )
 }
