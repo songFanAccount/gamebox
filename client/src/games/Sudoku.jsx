4fe36d3eb14a7b804b111a6f27b624b2ca54e350
harryJang <yunseokbb@gmail.com>
2023-08-07 23:45:45 +0900

    starting working on multiplayer sudoku

--- client/src/games/Sudoku.jsx
+++ client/src/games/Sudoku.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box } from '@mui/material'
 
 export default function Sudoku() {
+    const socket = global.socket
     return (
         <Box>Let's play sudoku!</Box>
     )
